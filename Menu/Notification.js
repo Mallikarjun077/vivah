b69953f1548259c5a12290466ed43989337d8ec2
mallikarjun <shirwalmallu@gmail.com>
2025-07-18 23:41:21 +0530

    commit

--- Menu/Notification.js
+++ Menu/Notification.js
@@ -9,7 +9,7 @@ const MatchScreen = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#F2F0E8" }}>
-      <Text style={{ fontSize: 20, color: "#1C170D" }}>Coming Soon!</Text>
+      <Text style={{ fontSize: 20, color: "#1C170D" }}>Coming Soon</Text>
     </View>
   );
 };
